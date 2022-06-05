2cf967ce718a660d11afae4d29da6ac04c3916c6
Rafael Perger <rafael.perger@gmail.com>
2022-06-05 15:50:27 +0200

    Count item properly

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,14 +1,27 @@
 import propTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import '../styles/ShoppingCart.css';
 
 const ShoppingCart = (props) => {
   ShoppingCart.propTypes = {
-    numOfItems: propTypes.number
+    cart: propTypes.array
   };
+
+  const [numOfItems, setNumOfItems] = useState(0);
+
+  useEffect(() => {
+    const countItems = () => {
+      props.cart.forEach((item) => {
+        setNumOfItems(numOfItems + item.amount);
+      });
+    };
+
+    countItems();
+  }, [props.cart]);
 
   return (
     <div className="shopping-cart">
-      <div className="item-num">You have {props.numOfItems} in your cart</div>
+      <div className="item-num">You have items {numOfItems} in your cart</div>
       <button className="checkout-btn">Checkout</button>
     </div>
   );
