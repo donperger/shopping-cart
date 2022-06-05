f541e7db478dffd18b8e630cd2463a412d368abd
Rafael Perger <rafael.perger@gmail.com>
2022-06-05 08:31:53 +0200

    Implement data loading

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,5 +1,40 @@
+import { useState, useEffect } from 'react';
+import ItemCrads from './ItemCards';
+
 const Store = () => {
-  return <div className="store-comp">Hello from store!</div>;
+  const [products, setProdutcs] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((json) => {
+        setProdutcs(products.concat(json));
+        console.log('Data loaded');
+      });
+  }, []);
+
+  const onClick = () => {
+    console.log('Clicked!');
+    console.log(products);
+  };
+
+  return (
+    <div className="store-comp">
+      <button onClick={onClick}>Click Me</button>
+      <div className="products-cont">
+        {products.map((product) => {
+          return (
+            <ItemCrads
+              key={product.id}
+              title={product.title}
+              price={product.price}
+              image={product.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Store;
