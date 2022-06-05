4f43981f26b4e61f989114a5446a69395c8f463c
Rafael Perger <rafael.perger@gmail.com>
2022-06-05 15:50:09 +0200

    Create add cart functionality

--- src/components/ItemCards.js
+++ src/components/ItemCards.js
@@ -1,11 +1,23 @@
 import '../styles/ItemCards.css';
 import propTypes from 'prop-types';
+import { useState } from 'react';
 
 const ItemCrads = (props) => {
   ItemCrads.propTypes = {
     title: propTypes.string,
     image: propTypes.string,
-    price: propTypes.number
+    price: propTypes.number,
+    addItemToCart: propTypes.func
+  };
+
+  const [amountOfItem, setAmountOfItem] = useState(1);
+
+  const onInputChange = (e) => {
+    setAmountOfItem(e.target.value);
+  };
+
+  const addToCart = () => {
+    props.addItemToCart(props.title, props.price, Number(amountOfItem));
   };
 
   return (
@@ -13,8 +25,10 @@ const ItemCrads = (props) => {
       <div className="title">{props.title}</div>
       <img className="product-img" src={props.image} alt="Product image" />
       <div className="price">{props.price} $</div>
-      <input type="number" name="numberOfPurchase" />
-      <button className="add-to-cart-btn">Add To Cart</button>
+      <input type="number" name="amountOfItem" value={amountOfItem} onChange={onInputChange} />
+      <button className="add-to-cart-btn" onClick={addToCart}>
+        Add To Cart
+      </button>
     </div>
   );
 };
