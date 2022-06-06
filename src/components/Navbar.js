c93e727cb9b8a8e6c5768c99ed752973eb3058f4
Rafael Perger <rafael.perger@gmail.com>
2022-06-06 16:20:13 +0200

    Style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,21 @@
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import gitHubIcon from '../img/github-icon-svgrepo-com.svg';
 
 const Navbar = () => {
   return (
     <nav>
       <div className="store-name">Spuer Random Super Store</div>
       <ul className="nav-list">
-        <li>
+        <li className="nav-elem">
           <Link to="/home">Home</Link>
         </li>
-        <li>
+        <li className="nav-elem">
           <Link to="/store">Store</Link>
         </li>
-        <li>
+        <li className="nav-elem">
           <a href="https://github.com/donperger">@donperger</a>
+          <img className="github-icon" src={gitHubIcon} alt="GitHub icon" />
         </li>
       </ul>
     </nav>
