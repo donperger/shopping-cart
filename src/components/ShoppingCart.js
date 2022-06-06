a09e5b7ed3ddb1f9700e3c6118b079c2fb22b111
Rafael Perger <rafael.perger@gmail.com>
2022-06-06 15:41:21 +0200

    Calculate and show total value of cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -8,14 +8,19 @@ const ShoppingCart = (props) => {
   };
 
   const [numOfItems, setNumOfItems] = useState(0);
+  const [displayList, setDisplayList] = useState(false);
+  const [cartValue, setCartValue] = useState(0);
 
   useEffect(() => {
     const countItems = () => {
-      let counter = 0;
+      let pieceCounter = 0;
+      let valueCounter = 0;
       props.cart.forEach((item) => {
-        counter += item.amount;
+        pieceCounter += item.amount;
+        valueCounter += item.amount * item.price;
       });
-      setNumOfItems(counter);
+      setNumOfItems(pieceCounter);
+      setCartValue(valueCounter);
     };
 
     const updateCartInLocalStorage = () => {
@@ -26,10 +31,40 @@ const ShoppingCart = (props) => {
     countItems();
   }, [props.cart]);
 
+  const showList = () => {
+    setDisplayList(true);
+  };
+
+  const hideList = () => {
+    setDisplayList(false);
+  };
+
   return (
     <div className="shopping-cart">
       <div className="item-num">You have items {numOfItems} in your cart</div>
-      <button className="checkout-btn">Checkout</button>
+      {!displayList && (
+        <button className="checkout-btn" onClick={showList}>
+          Checkout
+        </button>
+      )}
+      {displayList && (
+        <div className="checkout-list">
+          <ul>
+            {props.cart.map((item, index) => {
+              return (
+                <li key={index}>
+                  {item.amount} x {item.name}, {item.price}$ = {item.amount * item.price}$
+                </li>
+              );
+            })}
+            <li className="total-value">Total: {cartValue}$</li>
+          </ul>
+          <button className="proceed-btn">Proceed To Payment Options</button>
+          <button className="go-back-btn" onClick={hideList}>
+            Go Back To Shopping
+          </button>
+        </div>
+      )}
     </div>
   );
 };
