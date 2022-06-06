4f0d177733dd0b7bedb023793fa712e8d1283894
Rafael Perger <rafael.perger@gmail.com>
2022-06-06 21:41:08 +0200

    Style home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,18 @@
+import '../styles/Home.css';
+import homeImg from '../img/jomjakkapat-parrueng-qaUMOLJwb48-unsplash.jpg';
+
 const Home = () => {
-  return <div className="home-comp">Hello from Home</div>;
+  return (
+    <div className="home-comp">
+      <div className="welcome-text">
+        Welcome to Super Random Super Store! Check out our deals, and grab what you need!
+      </div>
+      <div className="home-img-cont">
+        <img className="home-img" src={homeImg} alt="Man in a shopping cart" />
+        <div className="photo-credit">Photo by Jomjakkapat Parrueng on Unsplash</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
