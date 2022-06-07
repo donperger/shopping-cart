ed3e9d948751ee5d7cfd331403b0814d5a7f438b
Rafael Perger <rafael.perger@gmail.com>
2022-06-07 07:26:58 +0200

    Update navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,19 @@ const Navbar = () => {
       <div className="store-name">Spuer Random Super Store</div>
       <ul className="nav-list">
         <li className="nav-elem">
-          <Link to="/home">Home</Link>
+          <Link id="home-link" to="/home">
+            Home
+          </Link>
         </li>
         <li className="nav-elem">
-          <Link to="/store">Store</Link>
+          <Link id="store-link" to="/store">
+            Store
+          </Link>
         </li>
         <li className="nav-elem">
-          <a href="https://github.com/donperger">@donperger</a>
+          <a id="github-link" href="https://github.com/donperger">
+            @donperger
+          </a>
           <img className="github-icon" src={gitHubIcon} alt="GitHub icon" />
         </li>
       </ul>
